7a924ce7bb12add122f0691e28246beca3082d9d
MAITITE MOHAMED <maititemohamed@gmail.com>
2023-02-01 15:44:11 +0100

    project structure

---
+++ walle-microservice/src/test/java/com/example/wallemicroservice/WalleMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wallemicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WalleMicroserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
